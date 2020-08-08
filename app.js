f14b79da63e408a84917759d679be117e7becd6f
shahaj90 <gshahaj@gmail.com>
2020-08-09 00:11:27 +0600

    File Upload

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-var passport = require('passport');
-var authenticate = require('./authenticate');
-var config = require('./config');
+var uploadRouter = require('./routes/uploadRouter');
+
 
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
@@ -45,12 +47,12 @@ app.use(express.urlencoded({
 app.use(passport.initialize());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
